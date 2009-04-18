52d1cd7be65e40ded03bdf5b88a056a3e805cd7b
Suguru Yamamoto <sgryjp@gmail.com>
2009-04-18 10:50:33 +0000

    v1.3.1

--- Azuki/AssemblyInfo.cs
+++ Azuki/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Resources;
 
 [assembly: ComVisible(true)]
 [assembly: Guid("272d419b-f573-42b4-9b0b-32cc5fd85a31")]
-[assembly: AssemblyVersion("1.3.0.*")]
+[assembly: AssemblyVersion("1.3.1.*")]

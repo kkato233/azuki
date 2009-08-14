a9eb39652e075bdb664e6b31d4b11ae6988e6f52
Suguru Yamamoto <sgryjp@gmail.com>
2009-08-14 04:46:45 +0000

    ドキュメント生成プロジェクトを改善

--- doc.bat
+++ doc.bat
@@ -2,20 +2,25 @@
 setlocal
 
 :: Sandcastle Help File Builder directory path
-set SHFB_DIR=C:\Program Files\Sandcastle Help File Builder
+set SHFB_DIR=%ProgramFiles%\Sandcastle Help File Builder
+
+:: set PATH
+set PATH_BACKUP=%PATH%
+set PATH=%PATH%;%SHFB_DIR%
 
 :: execute SHFB console version to ensure existence
-("%SHFB_DIR%\SandcastleBuilderConsole.exe" > NUL) 2> NUL
+(SandcastleBuilderConsole.exe > NUL) 2> NUL
 if not %ERRORLEVEL% == 1 (
 	echo Failed to execute SandcastleBuilderConsole.exe.
 	echo Please edit doc.bat and set install-path to SHFB_DIR variable.
-	echo Currently SHFB_DIR is set as next:
-	echo %SHFB_DIR%
 	echo.
 	goto :EOF
 )
 
 :: okay, SHFB exists. execute it.
 pushd doc
-"%SHFB_DIR%\SandcastleBuilderConsole.exe" api.shfb
+SandcastleBuilderConsole.exe api.shfb
 popd
+
+set PATH=%PATH_BACKUP%
+

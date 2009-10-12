41685ca12f7d60b7cc582e227b804bf8739060f6
Suguru Yamamoto <sgryjp@gmail.com>
2009-10-12 01:41:33 +0000

    [Ann]描画オプションダイアログがCompact Framework版でレイアウトが崩れないように

--- Ann/DrawingOptionForm.cs
+++ Ann/DrawingOptionForm.cs
@@ -1,4 +1,4 @@
-﻿// 2009-09-06
+﻿// 2009-10-12
 using System;
 using System.Drawing;
 using System.Windows.Forms;
@@ -10,15 +10,6 @@ namespace Sgry.Ann
 {
 	class DrawingOptionForm : Form
 	{
-		/*
-		 * Metrics:
-		 * form.padding-right: 16
-		 * form.padding-left: 16
-		 * leftPane.padding-left: 0
-		 * leftPane.width: 280
-		 * leftPane.padding-right: 16
-		 * (leftPane.right: 296)
-		 */
 		#region Fields
 		DrawingOption _DrawingOption;
 		int _TabWidth;
@@ -32,16 +23,9 @@ namespace Sgry.Ann
 			this._Azuki_Preview.Text = "#include <stdio.h>\n\nint main( int argc, char* argv[] ) {\n\tprintf( \"en: Hello, World!\\n\" );\n\tprintf( \"ja: \x3053\x3093\x306b\x3061\x306f\x3000\x4e16\x754c\xff01\\n\" );\n\treturn 0;\n}\n/*CR+LF*/\r\n/*LF*/\r";
 			this._Azuki_Preview.ClearHistory();
 			this._Azuki_Preview.Highlighter = Highlighters.Cpp;
-			this.AutoScaleMode = AutoScaleMode.Font;
-			this.Font = SystemInformation.MenuFont;
-			this.StartPosition = FormStartPosition.CenterParent;
-			this.AcceptButton = _Button_OK;
-			this.CancelButton = _Button_Cancel;
-#			else
-			this.Controls.Remove( _Label_Preview );
-			this.Controls.Remove( _Azuki_Preview );
-			this.ClientSize = new Size( 296, ClientSize.Height );
+			Font = SystemInformation.MenuFont;
 #			endif
+			//LayoutComponent();
 
 			Closing += Form_Closing;
 		}
@@ -161,7 +145,7 @@ namespace Sgry.Ann
 			// 
 			_Check_DrawsSpace.Location = new Point( 16, 12 );
 			_Check_DrawsSpace.Name = "_Check_DrawsSpace";
-			_Check_DrawsSpace.Size = new Size( 264, 17 );
+			_Check_DrawsSpace.Size = new Size( 224, 19 );
 			_Check_DrawsSpace.Text = "Draws &space";
 			_Check_DrawsSpace.Click += _CheckBox_CheckedChanged;
 			// 
@@ -169,7 +153,7 @@ namespace Sgry.Ann
 			// 
 			_Check_DrawsTab.Location = new Point( 16, 33 );
 			_Check_DrawsTab.Name = "_Check_DrawsTab";
-			_Check_DrawsTab.Size = new Size( 264, 17 );
+			_Check_DrawsTab.Size = new Size( 224, 19 );
 			_Check_DrawsTab.Text = "Draws &tab";
 			_Check_DrawsTab.Click += _CheckBox_CheckedChanged;
 			// 
@@ -177,7 +161,7 @@ namespace Sgry.Ann
 			// 
 			_Check_DrawsFullWidthSpace.Location = new Point( 16, 54 );
 			_Check_DrawsFullWidthSpace.Name = "_Check_DrawsFullWidthSpace";
-			_Check_DrawsFullWidthSpace.Size = new Size( 264, 17 );
+			_Check_DrawsFullWidthSpace.Size = new Size( 224, 19 );
 			_Check_DrawsFullWidthSpace.Text = "Draws &full width space (U+3000)";
 			_Check_DrawsFullWidthSpace.Click += _CheckBox_CheckedChanged;
 			// 
@@ -185,7 +169,7 @@ namespace Sgry.Ann
 			// 
 			_Check_DrawsEolCode.Location = new Point( 16, 75 );
 			_Check_DrawsEolCode.Name = "_Check_DrawsEolCode";
-			_Check_DrawsEolCode.Size = new Size( 264, 17 );
+			_Check_DrawsEolCode.Size = new Size( 224, 19 );
 			_Check_DrawsEolCode.Text = "Draws EO&L (end of line) code";
 			_Check_DrawsEolCode.Click += _CheckBox_CheckedChanged;
 			// 
@@ -193,7 +177,7 @@ namespace Sgry.Ann
 			// 
 			_Check_HighlightCurrentLine.Location = new Point( 16, 96 );
 			_Check_HighlightCurrentLine.Name = "_Check_HighlightCurrentLine";
-			_Check_HighlightCurrentLine.Size = new Size( 264, 17 );
+			_Check_HighlightCurrentLine.Size = new Size( 224, 19 );
 			_Check_HighlightCurrentLine.Text = "Highlights &current line";
 			_Check_HighlightCurrentLine.Click += _CheckBox_CheckedChanged;
 			// 
@@ -201,7 +185,7 @@ namespace Sgry.Ann
 			// 
 			_Check_ShowsLineNumber.Location = new Point( 16, 117 );
 			_Check_ShowsLineNumber.Name = "_Check_ShowsLineNumber";
-			_Check_ShowsLineNumber.Size = new Size( 264, 17 );
+			_Check_ShowsLineNumber.Size = new Size( 224, 19 );
 			_Check_ShowsLineNumber.Text = "Shows line &number";
 			_Check_ShowsLineNumber.Click += _CheckBox_CheckedChanged;
 			// 
@@ -209,46 +193,46 @@ namespace Sgry.Ann
 			// 
 			_Check_ShowsHRuler.Location = new Point( 16, 138 );
 			_Check_ShowsHRuler.Name = "_Check_ShowsHRuler";
-			_Check_ShowsHRuler.Size = new Size( 264, 17 );
+			_Check_ShowsHRuler.Size = new Size( 224, 19 );
 			_Check_ShowsHRuler.Text = "Shows &horizontal ruler";
 			_Check_ShowsHRuler.Click += _CheckBox_CheckedChanged;
 			// 
 			// _Label_TabWidth
 			// 
-			_Label_TabWidth.Location = new Point( 16, 142 );
+			_Label_TabWidth.Location = new Point( 16, 162 );
 			_Label_TabWidth.Name = "_Label_TabWidth";
-			_Label_TabWidth.Size = new Size( 80, 17 );
+			_Label_TabWidth.Size = new Size( 70, 19 );
 			_Label_TabWidth.Text = "Tab &width:";
 			// 
 			// _Num_TabWidth
 			// 
-			_Num_TabWidth.Location = new Point( 98, 157 );
+			_Num_TabWidth.Location = new Point( _Label_TabWidth.Right + 8, 159 );
 			_Num_TabWidth.Name = "_Num_TabWidth";
-			_Num_TabWidth.Size = new Size( 172, 24 );
+			_Num_TabWidth.Size = new Size( 62, 24 );
 			_Num_TabWidth.ValueChanged += _Num_TabWidth_ValueChanged;
 			// 
 			// _Button_OK
 			// 
-			_Button_OK.Location = new Point( 116, 185 );
+			_Button_OK.Location = new Point( 49, 192 );
 			_Button_OK.Name = "_Button_OK";
-			_Button_OK.Size = new Size( 75, 20 );
+			_Button_OK.Size = new Size( 90, 24 );
 			_Button_OK.Text = "OK";
 			_Button_OK.Click += _Button_OK_Click;
 			// 
 			// _Button_Cancel
 			// 
-			_Button_Cancel.Location = new Point( 197, 185 );
+			_Button_Cancel.Location = new Point( 143, 192 );
 			_Button_Cancel.Name = "_Button_Cancel";
-			_Button_Cancel.Size = new Size( 75, 20 );
+			_Button_Cancel.Size = new Size( 90, 24 );
 			_Button_Cancel.Text = "Cancel";
 			_Button_Cancel.Click += _Button_Cancel_Click;
 #			if !PocketPC
 			// 
 			// _Label_Preview
 			// 
-			_Label_Preview.Location = new Point( 296, 12 );
+			_Label_Preview.Location = new Point( 250, 9 );
 			_Label_Preview.Name = "_Label_Preview";
-			_Label_Preview.Size = new Size( 80, 17 );
+			_Label_Preview.Size = new Size( 80, 19 );
 			_Label_Preview.Text = "Preview:";
 			// 
 			// _Azuki_Preview
@@ -257,9 +241,9 @@ namespace Sgry.Ann
 			_Azuki_Preview.AcceptsTab = false;
 			_Azuki_Preview.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
 			_Azuki_Preview.Font = new Font( "Courier New", 10, FontStyle.Regular );
-			_Azuki_Preview.Location = new Point( 296, 27 );
+			_Azuki_Preview.Location = new Point( 250, 27 );
 			_Azuki_Preview.Name = "_Azuki_Preview";
-			_Azuki_Preview.Size = new Size( 244, 161 );
+			_Azuki_Preview.Size = new Size( 250, 188 );
 			_Azuki_Preview.TabStop = false;
 			_Azuki_Preview.ViewWidth = 328;
 #			endif
@@ -267,8 +251,18 @@ namespace Sgry.Ann
 			// DrawingOptionForm
 			// 
 			AutoScroll = true;
-			ClientSize = new Size( 560, 215 );
 			MinimizeBox = false;
+			AutoScaleDimensions = new SizeF( 96f, 96f );
+#			if !PocketPC
+			AutoScaleMode = AutoScaleMode.Dpi;
+			StartPosition = FormStartPosition.CenterParent;
+			AcceptButton = _Button_OK;
+			CancelButton = _Button_Cancel;
+			ClientSize = new Size( 516, 224 );
+#			else
+			AutoScaleMode = AutoScaleMode.Dpi;
+			ClientSize = new Size( 380, 224 );
+#			endif
 			Controls.Add( _Check_DrawsSpace );
 			Controls.Add( _Check_DrawsTab );
 			Controls.Add( _Check_DrawsFullWidthSpace );
@@ -280,11 +274,36 @@ namespace Sgry.Ann
 			Controls.Add( _Num_TabWidth );
 			Controls.Add( _Button_OK );
 			Controls.Add( _Button_Cancel );
+#			if !PocketPC
 			Controls.Add( _Azuki_Preview );
 			Controls.Add( _Label_Preview );
+#			endif
 			Name = "DrawingOptionForm";
 			Text = "Ann - Drawing Options";
 			ResumeLayout( false );
+		}
+
+		void LayoutComponent()
+		{
+			const int extra = 10;
+
+			using( IGraphics g = Plat.Inst.GetGraphics(Handle) )
+			{
+				_Check_DrawsSpace.Width
+					= g.MeasureText( _Check_DrawsSpace.Text ).Width + extra;
+				_Check_DrawsTab.Width
+					= g.MeasureText( _Check_DrawsTab.Text ).Width + extra;
+				_Check_DrawsFullWidthSpace.Width
+					= g.MeasureText( _Check_DrawsFullWidthSpace.Text ).Width + extra;
+				_Check_DrawsEolCode.Width
+					= g.MeasureText( _Check_DrawsEolCode.Text ).Width + extra;
+				_Check_HighlightCurrentLine.Width
+					= g.MeasureText( _Check_HighlightCurrentLine.Text ).Width + extra;
+				_Check_ShowsLineNumber.Width
+					= g.MeasureText( _Check_ShowsLineNumber.Text ).Width + extra;
+				_Check_ShowsHRuler.Width
+					= g.MeasureText( _Check_ShowsHRuler.Text ).Width + extra;
+			}
 		}
 		#endregion
 

1e12226bdfdf524f195f7ee5ffc358a5fe93c336
Suguru Yamamoto <sgryjp@gmail.com>
2010-03-21 07:26:16 +0000

    v1.5.1 (Ann 1.4.1)

--- Ann/AssemblyInfo.cs
+++ Ann/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 #endif
 
 [assembly: AssemblyCompany("YAMAMOTO Suguru")]
-[assembly: AssemblyCopyright("Copyright (C) 2008-2009, YAMAMOTO Suguru")]
+[assembly: AssemblyCopyright("Copyright (C) 2008-2010, YAMAMOTO Suguru")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.0.*")]
+[assembly: AssemblyVersion("1.4.1.*")]
--- Azuki/AssemblyInfo.cs
+++ Azuki/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 #endif
 
 [assembly: AssemblyCompany("YAMAMOTO Suguru")]
-[assembly: AssemblyCopyright("Copyright (C) 2007-2009, YAMAMOTO Suguru")]
+[assembly: AssemblyCopyright("Copyright (C) 2007-2010, YAMAMOTO Suguru")]
 
 [assembly: ComVisible(true)]
 [assembly: Guid("272d419b-f573-42b4-9b0b-32cc5fd85a31")]
-[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyVersion("1.5.1.*")]

22e4d0f367d191577268b1eff36f3263c1a013b0
Suguru Yamamoto <sgryjp@gmail.com>
2010-04-17 12:04:16 +0000

    v1.5.4 (Ann 1.4.4)

--- Ann/AssemblyInfo.cs
+++ Ann/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright (C) 2008-2010, YAMAMOTO Suguru")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.3.*")]
+[assembly: AssemblyVersion("1.4.4.*")]

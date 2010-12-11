14b019b12753573413af7fa08d3657df118c0d2f
Suguru Yamamoto <sgryjp@gmail.com>
2010-12-11 09:59:10 +0000

    ドキュメント自動生成処理をSHFB 1.8に対応させた。

--- doc.bat
+++ doc.bat
@@ -1,27 +1,6 @@
 @echo off
 setlocal
 
-:: Sandcastle Help File Builder directory path
-set SHFB_DIR=%ProgramFiles%\Sandcastle Help File Builder
-if not "%ProgramFiles(x86)%" == "" set SHFB_DIR=%ProgramFiles(x86)%\Sandcastle Help File Builder
-
-:: set PATH
-set PATH_BACKUP=%PATH%
-set PATH=%PATH%;%SHFB_DIR%
-
-:: execute SHFB console version to ensure existence
-(SandcastleBuilderConsole.exe > NUL) 2> NUL
-if not %ERRORLEVEL% == 1 (
-	echo Failed to execute SandcastleBuilderConsole.exe.
-	echo Please edit doc.bat and set install-path to SHFB_DIR variable.
-	echo.
-	goto :EOF
-)
-
-:: okay, SHFB exists. execute it.
 pushd doc
-SandcastleBuilderConsole.exe Document.shfb
+  msbuild  /p:Configuration=Release  Document.shfbproj
 popd
-
-set PATH=%PATH_BACKUP%
-

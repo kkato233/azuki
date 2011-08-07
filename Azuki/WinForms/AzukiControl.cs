926b2989b197548cffd9eed09bcaf2456cd53174
Suguru Yamamoto <sgryjp@gmail.com>
2011-08-07 05:37:25 +0000

    複数のフォームに AzukiControl を配置し、それらの Dispose メソッドを呼ばなかった場合にもアプリケーションが終了できるように改善

--- Azuki/WinForms/AzukiControl.cs
+++ Azuki/WinForms/AzukiControl.cs
@@ -1,7 +1,7 @@
 ﻿// file: AzukiControl.cs
 // brief: User interface for WinForms framework (both Desktop and CE).
 // author: YAMAMOTO Suguru
-// update: 2011-07-31
+// update: 2011-08-07
 //=========================================================
 using System;
 using System.Collections.Generic;
@@ -107,6 +107,9 @@ namespace Sgry.Azuki.WinForms
 
 			// setup default keybind
 			ResetKeyBind();
+
+			// install exit event handler to dispose resources propely
+			Application.ThreadExit += OnThreadExit;
 		}
 
 		/// <summary>
@@ -119,7 +122,15 @@ namespace Sgry.Azuki.WinForms
 			{
 				_Impl.Dispose();
 				_Impl = null;
+				Application.ThreadExit -= OnThreadExit;
 			}
+		}
+
+		void OnThreadExit( object sender, EventArgs e )
+		{
+			// if the GUI thread ended before this AzukiControl was disposed,
+			// dispose this to ensure that the child threads are terminated.
+			Dispose( true );
 		}
 
 		/// <summary>
